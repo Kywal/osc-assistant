b389e46476f234494f4819b0b5b1166a360853cb
Emanuel Kywal <emanuelkywal@outlook.com>
2025-05-06 21:56:53 -0300

    feat(backend): project basic structure

---
+++ backend/osc-assistant/src/main/java/us/dispatch/oscAssistant/OscAssistantApplication.java
@@ -0,0 +1,13 @@
+package us.dispatch.oscAssistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OscAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OscAssistantApplication.class, args);
+	}
+
+}
---
+++ backend/osc-assistant/src/test/java/us/dispatch/oscAssistant/OscAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package us.dispatch.oscAssistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OscAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
